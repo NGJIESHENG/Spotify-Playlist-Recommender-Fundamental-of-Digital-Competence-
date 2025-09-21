9d4e3c237c86941427e3f3f4de97cf688fe6eb6e
Jie Sheng <jasonng0315@gmail.com>
2025-09-21 15:35:54 +0800

    Add program banner with version and team info

---
+++ spotify.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int choice;
+
+    cout<<"===================================" <<endl;
+    cout<< " Spotify Playlist Recommender v1.0" <<endl;
+    cout<<" Contributor: Ng Jie Sheng, Pan Han Cheng" <<endl;
+    cout<<"===================================" <<endl;
+}
