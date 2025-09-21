f52ba0d3395d356848ebb3b9e6f9b5f680772efa
Jie Sheng <jasonng0315@gmail.com>
2025-09-21 20:25:23 +0800

    Add genre menu selection system

--- spotify.cpp
+++ spotify.cpp
@@ -8,4 +8,18 @@ int main(){
     cout<< " Spotify Playlist Recommender v1.0" <<endl;
     cout<<" Contributor: Ng Jie Sheng, Pan Han Cheng" <<endl;
     cout<<"===================================" <<endl;
+
+    cout<<"Choose a genre:" <<endl;
+    cout<<"1. Pop" <<endl;
+    cout<<"2. Rock" <<endl;
+    cout<<"3. Jazz" <<endl;
+    cout<<"4. Sad" <<endl;
+    cout<<"5. Workout"<<endl;
+    cout<<"6. EDM"<< endl;
+    cout<<"Enter your choice (1-6): ";
+    cin>> choice;
+
+
+    
+
 }
